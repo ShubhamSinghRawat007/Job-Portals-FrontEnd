f21167add5de09a0768151fe3b0574c2a93a9359
ShubhamSinghRawat007 <127177695+ShubhamSinghRawat007@users.noreply.github.com>
2024-12-01 18:18:25 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { NavBar } from './components/NavBar/NavBar'
-import { Search } from './components/SearchDiv/Search'
+import { NavBar } from './components/NavBar/Navbar.jsx'
+import { Search } from './components/SearchDiv/Search.jsx'
 import Home from './Pages/Home'
-import { Value } from './components/ValueDiv/Value'
-import { Footer } from './components/FooterDiv/Footer'
+import { Value } from './components/ValueDiv/Value.jsx'
+import { Footer } from './components/FooterDiv/Footer.jsx'
 
 
 export const App = () => {
